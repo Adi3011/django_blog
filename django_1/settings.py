61daff3233d9cd0888d70372127801b020691142
Aditya kumar <19bcs1084@gmail.com>
2021-07-25 13:06:07 +0530

    updated

--- django_1/settings.py
+++ django_1/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tqb#lj_984!7bk^q^3ud__x6z(v=u%(hz!z!fxdi^^vau(fd1#'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jangoblogapp.herokuapp.com']
 
 
 # Application definition
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -144,3 +145,13 @@ EMAIL_PORT=587
 EMAIL_USE_TLS=True
 EMAIL_HOST_USER=os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD=os.environ.get('EMAIL_PASS')
+
+
+ACCESS_KEY_ID = os.environ.get('ACCESS_KEY_ID')
+SECRET_ACCESS_KEY= os.environ.get('SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME= os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE =False
+
+AWS_DEFAULT_ACL= None
+DEFAULT_FILE_STORAGE =  'storages.backends.s3boto3.S3Boto3Storage'
